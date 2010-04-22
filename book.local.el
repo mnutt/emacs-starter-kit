acab0fd5d7459c800a31fa35b228cba2404d94ce
Geoffrey Grosenbach <boss@topfunky.com>
2010-04-22 12:38:47 -0700

    Bump up font size for 80 columns on higher resolution screen

--- book.local.el
+++ book.local.el
@@ -1,3 +1,3 @@
-;; Normal is 30pt. Screencastable is 26pt.
-(set-face-font 'default "-apple-inconsolata-medium-r-normal--30-0-72-72-m-0-iso10646-1")
-;; (color-theme-topfunky)
+;; Normal is 40pt. Screencastable is 26pt.
+(set-face-font 'default "-apple-inconsolata-medium-r-normal--40-0-72-72-m-0-iso10646-1")
+(color-theme-topfunky)
