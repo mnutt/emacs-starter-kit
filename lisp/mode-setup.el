2ac6318d27ded8c9f0a3dc170a46af52cc36fe3f
Michael Nutt <michael@nuttnet.net>
2019-03-20 16:30:26 -0400

    dynamically find prettier

--- lisp/mode-setup.el
+++ lisp/mode-setup.el
@@ -38,6 +38,11 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-hook 'js2-mode-hook 'ac-js2-mode)
 
+;; prettify javascript automatically
+(add-hook 'js2-mode-hook 'prettier-js-mode)
+;; ...but only if there's a prettierrc in the tree
+(setq prettier-js-command (concat dotfiles-dir "/../prettier"))
+
 ;; fix css mode
 (setq cssm-indent-level 2)
 (setq cssm-newline-before-closing-bracket t)
@@ -46,8 +51,8 @@
 (setq css-indent-offset 2)
 
 ;; sass mode
-(add-to-list 'auto-mode-alist '("\\.sass$" . sass-mode))
-(add-to-list 'auto-mode-alist '("\\.scss$" . sass-mode))
+(add-to-list 'auto-mode-alist '("\\.sass$" . css-mode))
+(add-to-list 'auto-mode-alist '("\\.scss$" . css-mode))
 
 ;; Turn on coloring for shell mode
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
@@ -64,8 +69,38 @@
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
+(setq web-mode-style-padding 2)
+(setq web-mode-script-padding 2)
 
 (setq js2-basic-offset 2)
 (setq js2-bounce-indent-p nil)
+
+;; typescript-mode
+(setq typescript-indent-level 2)
+
+;; to find node.js
+(setq exec-path (append exec-path '("/usr/local/bin")))
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+;; (add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+(add-to-list 'auto-mode-alist '("\\.capnp\\'" . capnp-mode))
 
 (provide 'mode-setup)
